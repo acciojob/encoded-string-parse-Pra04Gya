6fede969b97a0fc04abace9984656d690c23ab41
Pra04Gya <157507407+Pra04Gya@users.noreply.github.com>
2025-05-14 13:25:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const parseCode = (str) => {
-  // your code here
+    function parseCode(str) {
+    const parts = str.split(/0+/);
+    return {
+        firstName: parts[0],
+        lastName: parts[1],
+        id: parts[2]
+  };
+}
+
 };
 
 // Do not change the code below
