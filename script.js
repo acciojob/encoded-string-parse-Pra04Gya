2281208dff53f1681f3dea2211536d05c9b47095
Pra04Gya <157507407+Pra04Gya@users.noreply.github.com>
2025-06-17 18:28:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const parseCode = (str) => {
-    function parseCode(str) {
   const [firstName, lastName, id] = str.split(/0+/); 
   return { firstName, lastName, id };
-}
 };
 
+console.log(parseCode("John000Doe000123")); 
 // Do not change the code below
 const str = prompt("Enter str: ");
 alert(JSON.stringify(parseCode(str)));
